cd94cdc477f67dca94fe25b00c79793759a2eaf6
MoraswiChuene03 <MoraswiChuene03>
2025-07-02 21:19:49 +0200

    Added property add form, coded the routes for said form

--- routes/server.js
+++ routes/server.js
@@ -1,5 +1,11 @@
-Express = require("express");
-app = new Express();
+const bodyParser = require("body-parser");
+const session = require("express-session")
+const express = require("express");
+const app = new express();
+const multer = require("multer");
+const path = require("path")
+
+
 
 app.get('/complaint', (req, res) => {
     const tenant = req.session.tenant;
@@ -24,5 +30,54 @@ app.post('/complaint', (req, res)=> {
         return res.redirect('/complaint');
     }
 
-    req.success.success = 'Your complaint has been submitted successfully.';
+    req.session.success = 'Your complaint has been submitted successfully.';
+    return res.redirect('/complaint');
 })
+
+let propertyListings = [];
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({storage: storage});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'someSecret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { properties: propertyListings });
+});
+
+app.get('/add-property', (req, res) => {
+    res.render('add-property');
+});
+
+app.post('/add-property', upload.array('pics', 5), (req, res) => {
+    const { name, email, beds, baths, location, price } = req.body;
+    const images = req.files.map(file => '/uploads/' + file.filename);
+
+    const newListing = {
+        name,
+        email,
+        beds,
+        baths,
+        location,
+        price,
+        images
+    };
+
+    propertyListings.push(newListing);
+    res.redirect('/');
+});
